ca4fff3a2eafb054292af54dcb7ff30516f5e484
Heather Stoffels <Umdstarlet@gmail.com>
2020-04-24 17:53:57 -0500

    Got post route to talk to server side

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,42 +1,33 @@
 $(document).ready(onReady);
 
-function onReady(){
-    console.log('in onReady');
-    // add click listener for add task button
-    $("#addTask").on('click', addTask);
+function onReady() {
+  console.log("in onReady");
+  // add click listener for add task button
+  $("#addTask").on("click", addTask);
 
-    // run function that updates the dom and pulls current tasks
+  // run function that updates the dom and pulls current tasks
+  getAllTasks();
+}
+
+function addTask() {
+  console.log("in addTask");
+  // create object of task to send
+  let taskToAdd = {
+    taskInput: $("#task").val(),
+  };
+  console.log(taskToAdd);
+  //ajax POST request
+  $.ajax({
+    type: "POST",
+    url: "/todo",
+    data: taskToAdd,
+  }).then(function (response) {
+    console.log("back from POST", response);
     getAllTasks();
+  });
 }
 
-function addTask(){
-    console.log('in addTask');
-    // create object of task to send
-    let taskToAdd = {
-        taskInput: $('#task').val()
-    }
-
-    //ajax POST request
-    $.ajax({
-        type: "POST",
-        url: "/todo", 
-        data: taskToAdd
-    }).then(function(response){
-        console.log('back from POST', response);
-        getAllTasks();
-    })
-
-
-
-}
-
-function getAllTasks(){
-
-
-
-    //ajax get request
-
-
-
-    // make an el const, make for loop for response[i].something append to dom
+function getAllTasks() {
+  //ajax get request
+  // make an el const, make for loop for response[i].something append to dom
 }
--- server/server.js
+++ server/server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const app = express();
-const bodyParser = require('body-parser');
+const bodyParser = require("body-parser");
 const port = 5000;
 
-app.listen(port, ()=>{
-    console.log("listening on port: ", port);
-    
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(express.static("server/public"));
+
+app.post("/todo", (req, res) => {
+  console.log("in post on server side", req.body);
 });
 
-app.use(bodyParser.urlencoded({extended: true}));
-app.use(express.static('server/public'));
+app.listen(port, () => {
+  console.log("listening on port: ", port);
+});
