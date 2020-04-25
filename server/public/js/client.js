f1cc6dbc33973dce9f2a7dd86f2b803e7670b353
Heather Stoffels <Umdstarlet@gmail.com>
2020-04-25 10:33:52 -0500

    Finished writing PUT requests

--- server/public/js/client.js
+++ server/public/js/client.js
@@ -1,9 +1,12 @@
+// import { response } from "express";
+
 $(document).ready(onReady);
 
 function onReady() {
   console.log("in onReady");
   // add click listener for add task button
   $("#addTask").on("click", addTask);
+  $("#toggleComplete").on("click", completed);
 
   // run function that updates the dom and pulls current tasks
   getAllTasks();
@@ -13,7 +16,7 @@ function addTask() {
   console.log("in addTask");
   // create object of task to send
   let taskToAdd = {
-    taskInput: $("#task").val(),
+    task: $("#task").val(),
   };
   console.log(taskToAdd);
   //ajax POST request
@@ -28,6 +31,56 @@ function addTask() {
 }
 
 function getAllTasks() {
+  console.log("getting all tasks");
   //ajax get request
+  $.ajax({
+    type: "GET",
+    url: "/todo",
+  })
+    .then(function (responses) {
+      console.log("getting all tasks", responses);
+      let el = $("#showAllTasks");
+      el.empty();
+      console.log(responses);
+      responses.forEach((response) => {
+        console.log(response.complete);
+        //     // check for completeness (is the complete value true?)
+        if (response.complete) {
+          console.log("it's true");
+          el.append(`<li><s>${response.task}</s><button class="deleteButton data-id="${response.id}">Delete</button>
+         <button class="toggleComplete" data-id="${response.id}"
+         data-complete="${response.complete}">Complete</button></li>`);
+        } else {
+          console.log("this is false");
+          el.append(`<li>${response.task}<button class="deleteButton data-id="${response.id}">Delete</button>
+         <button class="toggleComplete" data-id="${response.id}"
+         data-complete="${response.complete}">Complete</button></li>`);
+        }
+        //     // if complete
+        //     // render list normally
+        //     // else, redner the list item with red background or striketrough
+      });
+    })
+    .catch(function (err) {
+      console.log("error getting tasks", err);
+    });
   // make an el const, make for loop for response[i].something append to dom
+}
+
+function completed() {
+  const id = $(this).data("id");
+  const completedTask = $(this).data("complete");
+  console.log(" in completed", id, completedTask);
+  $.ajax({
+    type: "PUT",
+    url: `/todo/${id}`,
+    data: { complete: !completedTask },
+  })
+    .then(function (response) {
+      console.log("back from server side PUT", response);
+      getAllTasks();
+    })
+    .catch(function (err) {
+      console.log("error updating PUT ", err);
+    });
 }
--- server/server.js
+++ server/server.js
@@ -2,14 +2,64 @@ const express = require("express");
 const app = express();
 const bodyParser = require("body-parser");
 const port = 5000;
+const pg = require("pg");
+const Pool = pg.Pool;
+
+const pool = new Pool({
+  database: "weekend-to-do-app", //name of database
+  host: "localhost", // where is database
+  port: 5432, // always default port
+  max: 10, // number of connections
+  idleTimeoutMillis: 10000, // 10 second timeout.
+});
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("server/public"));
 
-app.post("/todo", (req, res) => {
-  console.log("in post on server side", req.body);
-});
-
 app.listen(port, () => {
   console.log("listening on port: ", port);
+});
+
+app.post("/todo", (req, res) => {
+  console.log("in post on server side", req.body);
+  const query = `INSERT INTO "todo" ("task") VALUES ($1);`;
+  const values = [req.body.task];
+  pool
+    .query(query, values)
+    .then((results) => {
+      res.sendStatus(201);
+    })
+    .catch((err) => {
+      console.log("error with insert", err);
+      res.sendStatus(500);
+    });
+});
+
+app.get("/todo", (req, res) => {
+  console.log("in /todo GET");
+  const query = `SELECT * From "todo" ORDER BY "id";`;
+  pool
+    .query(query)
+    .then((results) => {
+      res.send(results.rows);
+    })
+    .catch((err) => {
+      console.log("error with server side GET", err);
+      res.sendStatus(500);
+    });
+});
+
+app.put("/todo/:id", (req, res) => {
+  console.log("/todo PUT", req.params.id, req.body);
+  const query = `UPDATE "todo" SET complete=$1 WHERE id = $2;`;
+  const values = [req.body.complete, req.params.id];
+  pool
+    .query(query, values)
+    .then((results) => {
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.log("error server side PUT ", err);
+      res.sendStatus(500);
+    });
 });
